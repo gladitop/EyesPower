30f15172ba7f28d4fcbbeceea30cbc9a3f50f779
Gladi <dam.almaev@gmail.com>
2019-12-30 23:35:15 +0300

    Time and Winloc

---
+++ Client/EyesPower/Time.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EyesPower
+{
+    static class Time
+    {
+        public struct tamer
+        {
+            int Hour;
+            int Minute;
+            int Second;
+
+            public void Embed(int hour = 0, int minute = 0, int second = 0)
+            {
+                Hour = hour;
+                Minute = minute;
+                Second = second;
+            }
+
+            void Tick()
+            {
+
+            }
+
+            public void Start()
+            {
+                Thread thread = new Thread(new ThreadStart(Work));
+                thread.Start();
+            }
+
+            void Work()
+            {
+                while (true)
+                {
+                    Second--;
+
+                    if (Second == 0)
+                    {
+                        if (Minute == 0)
+                        {
+                            if (Hour == 0)
+                            {
+                                Tick();
+                            }
+                            Minute = 59;
+                            Hour--;
+                        }
+                        Second = 59;
+                        Minute--;
+                    }
+
+                }
+            }
+        }
+    }
+}
---
+++ Winloc/Winloc/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Winloc
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string pass = "";
+        public bool Closing = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            pass = File.ReadAllText($"{Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles)}/EyesPower/Passworld.txt");
+        }
+
+        private void btpass_Click(object sender, RoutedEventArgs e)
+        {
+            var md5create = MD5.Create();
+            var md5 = md5create.ComputeHash(Encoding.UTF8.GetBytes(lbpass.Password));
+
+            if (pass == Convert.ToBase64String(md5))
+            {
+                MessageBox.Show("Пароль верный!", "Уря!", MessageBoxButton.OK, MessageBoxImage.Information);
+                Closing = true;
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Пароль неверный!", "Нееее!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (Closing == true)
+            {
+                e.Cancel = true;
+            }
+            else
+            {
+                e.Cancel = false;
+            }
+        }
+    }
+}
