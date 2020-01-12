d1336f831ecb39d60eb962d1fafef54e158b8549
damiralmaev <dam.almaev@gmail.com>
2020-01-12 20:54:24 +0300

    Забыл перемести!

---
+++ Winloc/Winloc/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Winloc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbpass = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btpass = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1033, 91);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ваше время закончилось!";
+            // 
+            // lbpass
+            // 
+            this.lbpass.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbpass.Location = new System.Drawing.Point(443, 241);
+            this.lbpass.Name = "lbpass";
+            this.lbpass.Size = new System.Drawing.Size(414, 49);
+            this.lbpass.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(256, 244);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(158, 44);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Пароль";
+            // 
+            // btpass
+            // 
+            this.btpass.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btpass.Location = new System.Drawing.Point(412, 360);
+            this.btpass.Name = "btpass";
+            this.btpass.Size = new System.Drawing.Size(231, 89);
+            this.btpass.TabIndex = 3;
+            this.btpass.Text = "Вести пароль";
+            this.btpass.UseVisualStyleBackColor = true;
+            this.btpass.Click += new System.EventHandler(this.btpass_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(1053, 561);
+            this.Controls.Add(this.btpass);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbpass);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "main";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox lbpass;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btpass;
+    }
+}
+
--- Winloc/Winloc/Form1.cs
+++ Winloc/Winloc/Form1.cs
@@ -4,60 +4,58 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.IO;
 using System.Security.Cryptography;
+using System.Windows.Forms;
 
 namespace Winloc
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Form1 : Form
     {
         public string pass = "";
         public bool Closing = false;
 
-        public MainWindow()
+        public Form1()
         {
             InitializeComponent();
             pass = File.ReadAllText($"{Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles)}/EyesPower/Passworld.txt");
         }
 
-        private void btpass_Click(object sender, RoutedEventArgs e)
+        private void btpass_Click(object sender, EventArgs e)
         {
             var md5create = MD5.Create();
-            var md5 = md5create.ComputeHash(Encoding.UTF8.GetBytes(lbpass.Password));
+            var md5 = md5create.ComputeHash(Encoding.UTF8.GetBytes(lbpass.Text));
 
             if (pass == Convert.ToBase64String(md5))
             {
-                MessageBox.Show("Пароль верный!", "Уря!", MessageBoxButton.OK, MessageBoxImage.Information);
+                MessageBox.Show("Пароль верный!", "Уря!", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 Closing = true;
                 this.Close();
             }
             else
             {
-                MessageBox.Show("Пароль неверный!", "Нееее!", MessageBoxButton.OK, MessageBoxImage.Error);
+                MessageBox.Show("Пароль неверный!", "Нееее!", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
+        }
+
+        public void Taskmsg()
+        {
+            
         }
 
         private void main_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
-            //if (Closing == true)
-            //{
-            //    e.Cancel = true;
-            //}
-            //else
-            //{
-            //    e.Cancel = false;
-            //}
+            if (Closing == true)
+            {
+                e.Cancel = true;
+            }
+            else
+            {
+                e.Cancel = false;
+            }
         }
     }
 }
